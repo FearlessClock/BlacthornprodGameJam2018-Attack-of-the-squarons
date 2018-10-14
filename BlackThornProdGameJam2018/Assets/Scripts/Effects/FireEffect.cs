c70f1784c128a44a7d18176e0984bbb7e314ca57
JackAlgera <jack.algera@live.com>
2018-10-14 17:24:25 +0200

    Added effects

---
+++ BlackThornProdGameJam2018/Assets/Scripts/Effects/FireEffect.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireEffect : SpellEffect {
+
+    // Damage when staying in shape
+    public float damInShape;
+    public float damInShapeProcTime;
+    private float damInShapeCurTime;
+
+    // Damage over time 
+    public float damOverTime;
+    public float damOverTimeProcTime;
+    private float damOverTimeCurTime;
+    public float damOverTimeDuration;
+    private float damOverTimeCurDuration;
+
+    public void InitEffect(float damInShape, float damInShapeProcTime, float damOverTime, float damOverTimeProcTime, float damOverTimeDuration)
+    {
+        this.damInShape = damInShape;
+        this.damInShapeProcTime = damInShapeProcTime;
+
+        this.damOverTime = damOverTime;
+        this.damOverTimeProcTime = damOverTimeProcTime;
+        this.damOverTimeDuration = damOverTimeDuration;
+
+        damInShapeCurTime = damInShapeProcTime;
+        damOverTimeCurTime = damOverTimeProcTime;
+        damOverTimeCurDuration = damOverTimeDuration;
+    }
+
+    public override void ApplyEffect()
+    {
+        // Damage in shape time
+        damInShapeCurTime -= Time.deltaTime;
+        if(damInShapeCurTime <= 0)
+        {
+            monster.GetComponent<MonsterController>().Damage(damInShape);
+            damInShapeCurTime = damInShapeProcTime;
+        }
+
+        // Damage over time
+        damOverTimeCurTime -= Time.deltaTime;
+        damOverTimeCurDuration -= Time.deltaTime;
+        if(damOverTimeCurDuration <= 0)
+        {
+            effectFinished = true;
+        }
+
+        if(damOverTimeCurTime <= 0)
+        {
+            monster.GetComponent<MonsterController>().Damage(damOverTime);
+            damOverTimeCurTime = damOverTimeProcTime;
+        }
+    }
+
+    public override void UpdateEffect()
+    {
+        damOverTimeCurDuration = damOverTimeDuration;
+    }
+
+    public override void LeaveShapeArea()
+    {
+        damInShape = 0;
+    }
+}
---
+++ BlackThornProdGameJam2018/Assets/Scripts/Effects/SpellEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SpellEffect : MonoBehaviour {
+
+    public GameObject monster;
+    public ElementalType spellType;
+    public int shapeId;
+
+    public bool effectFinished = false;
+
+    public abstract void ApplyEffect();
+    public abstract void UpdateEffect();
+    public abstract void LeaveShapeArea();
+}
