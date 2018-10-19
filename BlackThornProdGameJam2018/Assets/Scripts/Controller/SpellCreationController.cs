315677da45babc5a0b5d79f5dfdd2c613590eccf
Pieter ALGERA <pieter.algera@devinci.fr>
2018-10-19 10:30:57 +0200

    Added back button to the spell creation

---
+++ BlackThornProdGameJam2018/Assets/Scripts/Controller/SpellCreationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SpellCreationController : MonoBehaviour {
+    public int levelId;
+    public Image fadeOutPanel;
+    public FadeValues fadeValues;
+
+    public void LoadMainMenu()
+    {
+        StartCoroutine("FadeOut");
+    }
+
+    IEnumerator FadeOut()
+    {
+        float alpha = 0;
+        while (alpha < 1)
+        {
+            Debug.Log(alpha);
+            fadeOutPanel.color = new Color(0, 0, 0, alpha += Time.deltaTime * fadeValues.panelFadeOutSpeed);
+            yield return new WaitForSeconds(fadeValues.fadeTime);
+        }
+        SceneManager.LoadScene(levelId);
+    }
+}
