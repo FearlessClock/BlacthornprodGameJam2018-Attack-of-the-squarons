0788b9798f6c3f8b44a7e8f0e46c0fb4dab23b4a
Pieter ALGERA <pieter.algera@devinci.fr>
2018-10-20 09:01:31 +0200

    Added end game

---
+++ BlackThornProdGameJam2018/Assets/Scripts/EndGameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameController : MonoBehaviour {
+
+    public GameObject canvas;
+    public Transform monsterHolder;
+    private bool gameFinished;
+    // Use this for initialization
+	void Start () {
+        gameFinished = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(monsterHolder.childCount <= 0)
+        {
+            canvas.SetActive(true);
+            gameFinished = true;
+        }
+	}
+}
