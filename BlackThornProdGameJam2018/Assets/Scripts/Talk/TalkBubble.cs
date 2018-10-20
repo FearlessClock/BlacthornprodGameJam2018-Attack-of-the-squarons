145ed011d38ce75295ac29e49d5d0dc0ca931191
Pieter ALGERA <pieter.algera@devinci.fr>
2018-10-20 10:20:27 +0200

    Added talking

---
+++ BlackThornProdGameJam2018/Assets/Scripts/Talk/TalkBubble.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TalkBubble : MonoBehaviour {
+
+    public string[] words;
+    public int currentPosition;
+    public TextMeshProUGUI text;
+    public GameObject UI;
+    // Use this for initialization
+    void Start()
+    {
+        text.text = words[currentPosition];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NextBubble()
+    {
+        Debug.Log("Wesh");
+        currentPosition++;
+        if(currentPosition >= words.Length)
+        {
+            UI.SetActive(false);
+        }
+        text.text = words[currentPosition];
+    }
+}
