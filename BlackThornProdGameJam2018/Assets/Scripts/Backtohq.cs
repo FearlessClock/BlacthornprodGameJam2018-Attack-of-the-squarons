971a14f55ee74f844adbabee64d979adb46f9af5
Pieter ALGERA <pieter.algera@devinci.fr>
2018-10-20 12:00:08 +0200

    Help

---
+++ BlackThornProdGameJam2018/Assets/Scripts/Backtohq.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Backtohq : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BackToHQ()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
